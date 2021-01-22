76f39ccc3b4fd174db7b6ef571512b72a7dadbc8
Antony Rafael <depaulacostaantony@gmail.com>
2021-01-22 18:17:28 -0300

    Navegação prev-nav

--- js/slide.js
+++ js/slide.js
@@ -9,6 +9,10 @@ export default class Slide {
     }
   }
 
+  transition(active) {
+    this.slide.style.transition = active ? 'transform .3s' : '';
+  }
+
   moveSlide(distX) {
     this.dist.movePosition = distX;
     this.slide.style.transform = `translate3d(${distX}px,0,0)`
@@ -31,7 +35,7 @@ export default class Slide {
 
     }
     this.wrapper.addEventListener(moveType, this.onMove)
-
+    this.transition(false)
   }
 
   onMove(event) {
@@ -44,6 +48,19 @@ export default class Slide {
     const moveType = (event.type === 'mouseup') ? 'mousemove' : 'touchmove'
     this.wrapper.removeEventListener(moveType, this.onMove);
     this.dist.finalPosition = this.dist.movePosition;
+    this.transition(true)
+    this.changeSlideOnEnd();
+
+  }
+
+  changeSlideOnEnd() {
+    if (this.dist.movement > 120 && this.index.next !== undefined) {
+      this.activeNextSlide();
+    } else if (this.dist.movement < -120 && this.index.prev !== undefined) {
+      this.activePrevSlide();
+    } else {
+      this.changeSlide(this.index.active)
+    }
   }
 
   addSlideEvents() {
@@ -77,12 +94,12 @@ export default class Slide {
 
   slideIndexNav(index) {
     const last = this.slideArray.length -1;
-
     this.index = {
       prev: (index) ? index -1 : undefined,
       active: index,
       next: (index === last) ? undefined : index + 1
     }
+    return index;
   }
 
   changeSlide(index) {
@@ -92,8 +109,23 @@ export default class Slide {
     this.dist.finalPosition = activeSlide.position
   }
 
+  // Navegação
+  
+  activePrevSlide() {
+    if (this.index.prev !== undefined) {
+      this.changeSlide(this.index.prev)
+    }
+  }
+
+  activeNextSlide() {
+    if (this.index.next !== undefined) {
+      this.changeSlide(this.index.next)
+    }
+  }
+
   init() {
     this.bindEvents();
+    this.transition(true);
     this.addSlideEvents();
     this.slidesConfig();
     return this;
